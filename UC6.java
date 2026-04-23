1e19ffd4724a66195de2c67111b49aa4bbfe2cdc
Mohammed Faizan <mdfaizanansari1187@gmail.com>
2026-04-23 14:37:33 +0530

    Place Move on Board

---
+++ UC6.java
@@ -0,0 +1,38 @@
+public class TicTacToeUC6 {
+
+    // Method to place symbol on board
+    public static void placeMove(char[][] board, int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    // Method to display board
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Sample board
+        char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+        };
+
+        int row = 1;
+        int col = 1;
+        char symbol = 'X';
+
+        // Place move
+        placeMove(board, row, col, symbol);
+
+        // Display updated board
+        System.out.println("Updated Board:");
+        printBoard(board);
+    }
+}
